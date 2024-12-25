62e306f960ff485adf1d0ad58a34ffec096a03da
mohdels <mohammed.elsayed2@mail.mcgill.ca>
2024-12-25 10:09:55 -0500

    fixed email service #57

--- MyDailyHadith-Backend/config.py
+++ MyDailyHadith-Backend/config.py
@@ -9,6 +9,6 @@ DB_NAME = os.getenv("MONGO_DB_NAME")
 
 # Email configuration
 SMTP_SERVER= "smtp.gmail.com"
-SMTP_PORT = 597
+SMTP_PORT = 587
 EMAIL_ADDRESS = os.getenv('EMAIL_ADDRESS')
 EMAIL_PASSWORD = os.getenv('EMAIL_PASSWORD')
